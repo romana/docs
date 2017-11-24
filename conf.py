cbb73c662cea4ed3f0350b4b40f66ac9db4ab488
Chris Marino <chris@paninetworks.com>
2017-11-24 13:30:38 -0800

    Adding gAnalytics

--- conf.py
+++ conf.py
@@ -29,7 +29,10 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinxcontrib.googleanalytics', ]
+
+# google analytics
+googleanalytics_id = 'UA-86901810-1'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
